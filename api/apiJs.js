f5298db72b71580925694572ffd72eb5b057e2a5
artimerz <artimerz@ukr.net>
2020-12-26 01:43:38 +0200

    made routing

--- api/apiJs.js
+++ api/apiJs.js
@@ -1,8 +1,14 @@
-let express = require('express');
-let app = express();
-const envy = require('./mainClass.js');
+const express = require('express');
+const app = express();
 
+const envy = require('./mainClass.js');
 envy.get();
+
+const postRoutes = require('./Routing.js');
+app.use(express.static('/public'), postRoutes);
+
+const other = require('./otherAsks.js');
+app.use(express.static('/public'), other);
 
 function errorHandler(err, req, res, next) {
   if (res.headersSent) {
@@ -11,56 +17,6 @@ function errorHandler(err, req, res, next) {
   res.status(500);
   res.render('error', { error: err });
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.get('/', function (req, res, next) {
-  fs.readFile('/file-does-not-exist', function (err, data) {
-    if (err) {
-      next(err)
-    } else {
-      res.send(data)
-    }
-  })
-});
-
-app.get('/user/:id', async function (req, res, next) {
-  let user = await getUserById(req.params.id);
-  res.send(user)
-});
-
-app.get('/', [
-  function (req, res, next) {
-    fs.writeFile('/inaccessible-path', 'data', next)
-  },
-function (req, res) {
-  res.send('OK')
-}
-]);
-
-app.get('/', [
-  function (req, res, next) {
-    fs.readFile('/maybe-valid-file', 'utf-8', function (err, data) {
-      res.locals.data = data;
-      next(err)
-    })
-  },
-function (req, res) {
-  res.locals.data = res.locals.data.split(',')[1];
-  res.send(res.locals.data)
-}
-]);
-
-/*const db = require('db');
-db.connect({
-  port: process.env.PORT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-});*/
-
 
 app.listen(3000, () => {
 console.log("App server started!");
