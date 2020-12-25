aad1d445bf38bdce0e5cca977508763c64d514bc
artimerz <artimerz@ukr.net>
2020-12-25 23:45:29 +0200

    done previous ex

---
+++ api/mainClass.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+class Config {
+  get(param, defVal){
+    if(process.env[param]){
+      return process.env[param];
+    }
+    return defVal;
+  }
+}
+
+module.exports = new Config;
