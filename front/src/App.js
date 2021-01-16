2dfe19665afe5bc5c93ad29177d961bd09a58fd3
artimerz <artimerz@ukr.net>
2021-01-16 14:02:00 +0200

    add class in app.js

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,19 @@ import Header from './Header'
 import Middle from './Middle'
 import Left from './Left'
 
-function App () {
-  return (
-    <div>
-      <header><Header /></header>
-      <div><Left /></div>
-      <div><Middle /></div>
-    </div>
-  )
+class App extends React.Component {
+  /* state = {
+
+  }; */
+  render () {
+    return (
+      <div>
+        <header><Header /></header>
+        <div><Left /></div>
+        <div><Middle /></div>
+      </div>
+    )
+  }
 }
 
 export default App
