d2675e7823efa6a6b49e769820774d95c39e9699
artimerz <artimerz@ukr.net>
2021-02-05 13:32:16 +0200

    made func that check author of post

--- api/routes/Routing.js
+++ api/routes/Routing.js
@@ -4,14 +4,15 @@ require('dotenv').config()
 const authGetPost = require('../routes/userOwnPost')
 const userID = 'id'
 const table = 'posts'
-const postID = 'userID'
+const postID = 'userid'
+const db = require('../routes/forDB')
 
-router.get('/all', async (req, res) => {
+router.get('/all-posts', async (req, res) => {
   const posts = await db('posts').select('*')
   res.json(posts)
 })
 
-router.post('/create', (req, res) => {
+router.post('/create-post', (req, res) => {
   //Create new post
 })
 
@@ -22,14 +23,16 @@ router.get('/:id', (req, res) => {
 // router.use(authGetPost)
 
 router.put('/edit-post/:id', [authGetPost(userID, table, postID)], (req, res) => {
-  //console.log(req.user[0])
   res.json({
     message: 'you can edit post'
   })
   //Edit current post
 })
 
-router.delete('/delete-post/:id', (req, res) => {
+router.delete('/delete-post/:id', [authGetPost(userID, table, postID)], (req, res) => {
+  res.json({
+    message: 'you can delete post'
+  })
   //Delete current post
 })
 
--- api/routes/userOwnPost.js
+++ api/routes/userOwnPost.js
@@ -3,10 +3,11 @@ const db = require('../routes/forDB')
 const authGetPost = (mainTask, table, lastTask) => {
   return async (req, res, next) => {
     const element = await db(table).where({id: req.params.id}).first()
-
-    if(!(req.user.eval(mainTask) === element.eval(lastTask))){
+    if(!(req.user[0][mainTask] === element[lastTask])){
       res.status(401)
-      return res.send('Not allowed')
+      return res.json({
+        message: 'Not allowed'
+      })
     }
 
     next()
