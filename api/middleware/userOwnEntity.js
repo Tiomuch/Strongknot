57cadbad0b6bb3310ca3864c6f827f614f137cf1
artimerz <artimerz@ukr.net>
2021-02-21 18:51:47 +0200

    one small thing

--- api/middleware/userOwnEntity.js
+++ api/middleware/userOwnEntity.js
@@ -4,7 +4,7 @@ const authGetEntity = (mainTask, table, lastTask) => {
   return async (req, res, next) => {
     const element = await db(table).where({id: req.params.id}).first()
 
-    if(!(req.user[0][mainTask] === element[lastTask])){
+    if(req.user[0][mainTask] !== element[lastTask]){
       res.status(403)
       return res.json({
         message: 'Not allowed'
